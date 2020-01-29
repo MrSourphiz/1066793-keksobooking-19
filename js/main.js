3145dc6ed5ea2326df3197faa326cdda873ea034
MrSourphiz <verymadball@gmail.com>
2020-01-30 00:40:12 +0800

    Добавляет генерацию массива объектов

--- js/main.js
+++ js/main.js
@@ -7,6 +7,8 @@ var PLACE_PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http:
 
 var map = document.querySelector('.map');
 map.classList.remove('map--faded');
+var mapPinsElement = map.querySelector('.map__pins');
+var templatePin = document.querySelector('#pin').content.querySelector('.map__pin');
 
 var generateRandomNumber = function (min, max) {
   var randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
@@ -57,36 +59,52 @@ var generateRandomArr = function (length, arr) {
   return valueArr;
 };
 
-var generatePlaceArr = function (place) {
+var generatePlaceArr = function (length) {
+  var maxLength = length;
   var placeArr = [];
 
-  while (placeArr.length < 8) {
+  while (placeArr.length < maxLength) {
+    var place = {
+      author: {
+        avatar: 'img/avatars/user0' + generateRandomNumber(1, 8) + '.png'
+      },
+      offer: {
+        title: 'заголовок предложения',
+        addres: location.x + ', ' + location.y,
+        price: generateRandomNumber(500, 3200),
+        type: generateRandomArr(1, PLACE_TYPE),
+        rooms: generateRandomNumber(1, 4),
+        guests: generateRandomNumber(1, 4),
+        checkin: generateRandomArr(1, PLACE_TIME),
+        checkout: generateRandomArr(1, PLACE_TIME),
+        features: generateRandomArr(7, PLACE_FEATURES),
+        descriprion: 'строка с описанием',
+        photos: generateRandomArr(3, PLACE_PHOTOS)
+      },
+      location: {
+        x: generateRandomNumber(50, 1150),
+        y: generateRandomNumber(130, 630)
+      }
+    };
     placeArr.push(place);
   }
   return placeArr;
 };
 
-var place = {
-  author: {
-    avatar: 'img/avatars/user0' + generateRandomNumber(1, 8) + '.png'
-  },
-  offer: {
-    title: 'заголовок предложения',
-    addres: '{{location.x}}, {{location.y}}',
-    price: generateRandomNumber(500, 3200),
-    type: generateRandomArr(1, PLACE_TYPE),
-    rooms: generateRandomNumber(1, 4),
-    guests: generateRandomNumber(1, 4),
-    checkin: generateRandomArr(1, PLACE_TIME),
-    checkout: generateRandomArr(1, PLACE_TIME),
-    features: generateRandomArr(7, PLACE_FEATURES),
-    descriprion: 'строка с описанием',
-    photos: generateRandomArr(3, PLACE_PHOTOS)
-  },
-  location: {
-    x: generateRandomNumber(50, 1150),
-    y: generateRandomNumber(130, 630)
-  }
+generatePlaceArr(8);
+
+var renderPlace = function (place) {
+  var placeElement = templatePin.cloneNode(true);
+  placeElement.querySelector('.map__pin').style = 'left: ' + (place.location.x + 40) + 'px; top: ' + (place.location.y + 40) + 'px;';
+  placeElement.getElementsByTagName('img').src = place.author.avatar;
+  placeElement.getElementsByTagName('img').alt = place.offer.title;
 };
 
-generatePlaceArr(place);
+var fragment = document.createDocumentFragment();
+var placeArr = generatePlaceArr(8);
+for (var i = 0; i < placeArr.length; i++) {
+  fragment.appendChild(renderPlace(placeArr[i]));
+}
+
+mapPinsElement.appendChild(fragment);
+
