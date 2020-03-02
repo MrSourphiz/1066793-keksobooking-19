1e7ba3955bc815be3e4033acf2bddafb4ab3c6e7
MrSourphiz <verymadball@gmail.com>
2020-03-02 23:16:08 +0800

    Добавляет фильтр по преимуществам

--- js/filter.js
+++ js/filter.js
@@ -9,13 +9,15 @@
   var rooms = mapFilters.querySelector('#housing-rooms');
   var guests = mapFilters.querySelector('#housing-guests');
   var price = mapFilters.querySelector('#housing-price');
+  var features = mapFilters.querySelector('#housing-features');
+
 
   var filter = function (dataArray) {
     if (dataArray.length > window.constants.PIN_QUANTITY) {
       dataArray.length = window.constants.PIN_QUANTITY;
     }
     var result = dataArray.filter(function (data) {
-      return filteredByType(type.value, data) && filteredByPrice(price.value, data) && filteredByRooms(rooms.value, data) && filteredByGuests(guests.value, data);
+      return filteredByType(type.value, data) && filteredByPrice(price.value, data) && filteredByRooms(rooms.value, data) && filteredByGuests(guests.value, data) && filteredByFeatures(data);
     });
 
     return result;
@@ -50,6 +52,18 @@
       default:
         return dataArray;
     }
+  };
+
+  var filteredByFeatures = function (dataArray) {
+    var checkedFeatures = features.querySelectorAll('input[type=checkbox]:checked');
+    var arrayOfCheckedFeatures = Array.from(checkedFeatures);
+    var checkedValue = arrayOfCheckedFeatures.map(function (feature) {
+      return feature.value;
+    });
+
+    return checkedValue.every(function (feature) {
+      return dataArray.offer.features.includes(feature);
+    });
   };
 
   window.filter = {
